bbbecb69393fc2adec7f4a8a0dcaeb2535c8df12
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-03-22 16:51:01 +0100

    feat: création projet react et liaison avec symfony

---
+++ frontlibrary/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './component/MyComponent.js';
+
+function App() {
+  return (
+      <MyComponent />
+  );
+}
+
+export default App;

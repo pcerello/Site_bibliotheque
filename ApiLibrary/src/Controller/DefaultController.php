2df6adc96aa4fe3318f9ed97d584cd6602ef9c21
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-03-22 16:51:01 +0100

    feat: création projet react et liaison avec symfony

---
+++ ApiLibrary/src/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_default')]
+    public function index(): Response
+    {
+        /*return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);*/
+        
+        $data = [
+            'controller_name' => 'DefaultController',
+        ];
+        
+        $response = new Response(json_encode($data), 200, [
+            'Content-Type' => 'application/json'
+        ]);
+        
+        return $response;
+        
+    }
+}

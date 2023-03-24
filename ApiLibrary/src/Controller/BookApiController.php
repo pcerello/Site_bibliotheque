1ef0e3f418571f24257d0aab715dca418acb83ef
Younes El Mesaoudi Meftah <younes.el-mesaoudi-meftah@etu.u-bordeaux.fr>
2023-03-24 12:21:31 +0100

    feat: :sparkles: API - Prise en compte des paramètres "author" et "max" lors de la recherche de livres

--- ApiLibrary/src/Controller/BookApiController.php
+++ ApiLibrary/src/Controller/BookApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations\View;
 use Doctrine\ORM\EntityManagerInterface;
@@ -30,15 +31,33 @@ class BookApiController extends AbstractController
      * Return a list of books
      * @return mixed
      */
-    public function listBook(EntityManagerInterface $entityManager)
+    public function listBook(Request $request, EntityManagerInterface $entityManager)
     {
         $query = $entityManager->createQueryBuilder()
-        ->select('b.id, b.title, b.picture, b.language, b.nbrPages, b.resume, b.year, c.name, e.name')
-        ->from('\App\Entity\Book', 'b')
-        ->innerJoin('\App\Entity\Category', 'c', Expr\Join::WITH, 'b.category = c.id')
-        ->innerJoin('\App\Entity\Editor', 'e', Expr\Join::WITH, 'b.editor = e.id')
-        ->getQuery();
+        ->select('b.id, b.title')
+        ->from('\App\Entity\Book', 'b');
 
+        /* Gestion des paramètres de la query string de la requête */
+        $author_name = $request->get('author');
+        // Nombre maximum de livres
+        $max = $request->get('max');
+        // Vérification des valeurs des paramètres
+        if ($author_name) {
+            $query
+            ->innerJoin('\App\Entity\Write', 'w', Expr\Join::WITH, 'b.id = w.idBook')
+            ->innerJoin('\App\Entity\Author', 'a', Expr\Join::WITH, 'w.idAuthor = a.id')
+            ->where("a.name LIKE :author_name")
+            ->setParameter('author_name', "%$author_name%");
+        }
+        if ($max && is_numeric($max)) {
+            // Si la valeur est négative
+            if ($max < 0) {
+                return $this->json(["message" => "Nombre maximum de pages invalide"], 400);
+            } else {
+                $query->setMaxResults($max);
+            }
+        }
+        $query = $query->getQuery();
         $books = $query->getResult();
 
         return $this->json($books, 200);

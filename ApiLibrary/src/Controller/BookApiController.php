5992e4f64a28a434c28d9982a8556384dd41d1f0
Younes El Mesaoudi Meftah <younes.el-mesaoudi-meftah@etu.u-bordeaux.fr>
2023-03-24 10:08:25 +0100

    feat: :sparkles: API - Accéder à un livre selon son ID

--- ApiLibrary/src/Controller/BookApiController.php
+++ ApiLibrary/src/Controller/BookApiController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations\View;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr;
 
 #[Route("/api")]
 #[OA\Tag("Books")]
@@ -37,5 +38,43 @@ class BookApiController extends AbstractController
         $books = $query->getResult();
 
         return $this->json($books, 200);
+    }
+
+    #[OA\Get(summary: "Get a book")]
+    #[OA\Response(
+        response: 200,
+        description: "Get a book",
+        content: new OA\JsonContent(
+            ref: "#/components/schemas/BookInfos"
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Book not found"
+    )]
+    #[View(serializerGroups: ["books_infos"])]
+    #[Route("/books/{id}", methods: ["GET"])]
+    /**
+     * Return a book
+     * @return mixed
+     */
+    public function bookShow(EntityManagerInterface $entityManager, int $id)
+    {
+        $query = $entityManager->createQueryBuilder()
+        ->select('b.id, b.title, b.picture, b.language, b.nbrPages, b.resume, b.year, c.name, e.name')
+        ->from('\App\Entity\Book', 'b')
+        ->where('b.id = :id')
+        ->setParameter('id', $id)
+        ->innerJoin('\App\Entity\Category', 'c', Expr\Join::WITH, 'b.category = c.id')
+        ->innerJoin('\App\Entity\Editor', 'e', Expr\Join::WITH, 'b.editor = e.id')
+        ->getQuery();
+
+        $reader = $query->getOneOrNullResult();
+
+        if (!$reader) {
+            return $this->json(["message" => "Reader not found"], 404);
+        }
+
+        return $this->json($reader, 200);
     }
 }

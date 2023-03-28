b2b18d097341a1adbc10bfde6a1da22b251e86c3
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-03-28 11:27:10 +0200

    feat: :construction: barre de recherche liste author mais affichage livre non fonctionnel

--- frontlibrary/src/component/SearchEngine.js
+++ frontlibrary/src/component/SearchEngine.js
@@ -1,23 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AuthorBooks from "./AuthorBooks";
 
 function SearchEngine() {
-  
+  const [authorName, setAuthorName] = useState("");
+  const [suggestedAuthors, setSuggestedAuthors] = useState([]);
+  const [selectedAuthor, setSelectedAuthor] = useState(null);
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setAuthorName(value);
+    fetch(`http://localhost:8000/api/authors?name=${value}&max=5`, {
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setSuggestedAuthors(data);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSelectedAuthor(authorName);
+
+  };
+
+  const handleAuthorClick = (author) => {
+    setSelectedAuthor(author.name);
+  };
+
   return (
-    <form className="bg-sky-800 pt-16 pb-16">
-      <input
-        type="text"
-        className="p-1 text-color-hover"
-        placeholder="Rechercher un auteur"
-      />
-      <button
-        type="submit"
-        className="p-1 px-6 bg-color hover:bg-color-hover text-white"
-        
-      >
-        <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-      </button>
-    </form>
+    <div>
+      {selectedAuthor ? (
+        <AuthorBooks authorName={selectedAuthor} />
+      ) : (
+        <form className="bg-sky-800 pt-16 pb-16" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="p-1 text-color-hover"
+            placeholder="Rechercher un auteur"
+            value={authorName}
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            className="p-1 px-6 bg-color hover:bg-color-hover text-white"
+            onClick={() => setSelectedAuthor(authorName)}
+          >
+            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+          </button>
+          {suggestedAuthors.length > 0 && (
+            <ul className="bg-gray-100 text-gray-800">
+              {suggestedAuthors.map((author) => (
+                <li
+                  key={author.id}
+                  onClick={() => handleAuthorClick(author)}
+                >
+                  {author.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </form>
+      )}
+    </div>
   );
 }
 

54f4aeafe485c03b9165216860fd9d60c7b2da6d
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-03-28 10:59:01 +0200

    feat: :construction: api, recherche author

--- ApiLibrary/src/Controller/BookApiController.php
+++ ApiLibrary/src/Controller/BookApiController.php
@@ -38,6 +38,19 @@ class BookApiController extends AbstractController
         required: false,
         example: "Harry Potter"
     )]
+    #[OA\Parameter(
+        name: "date",
+        in: "query",
+        description: "Sort by date",
+        required: false,
+        example: "DESC"
+    )]
+    #[OA\Parameter(
+        name: "idAuthor",
+        in: "query",
+        description: "Author id",
+        required: false,
+        example: 1)]
     #[OA\Response(
         response: 200,
         description: "List of books",
@@ -58,6 +71,8 @@ class BookApiController extends AbstractController
         $author = $request->query->get('author');
         $max = $request->query->get('max');
         $title = $request->query->get('title');
+        $date = $request->query->get('date');
+        $idAuthor = $request->query->get('idAuthor');
 
         if ($title) {
             $queryBuilder->andWhere('b.title LIKE :title')
@@ -69,11 +84,24 @@ class BookApiController extends AbstractController
                 ->setParameter('author', "%$author%");
         }
 
+        if ($idAuthor) {
+            $queryBuilder->andWhere('a.id = :idAuthor')
+                ->setParameter('idAuthor', $idAuthor);
+        }
+
         if ($max && is_numeric($max)) {
             if ($max < 0) {
                 return $this->json(["message" => "Nombre maximum de pages invalide"], 400);
             } else {
                 $queryBuilder->setMaxResults($max);
+            }
+        }
+
+        if ($date) {
+            if ($date === "DESC") {
+                $queryBuilder->orderBy('b.year', 'DESC');
+            } else {
+                return $this->json(["message" => "Tri invalide"], 400);
             }
         }
 
---
+++ ApiLibrary/src/Repository/AuthorRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Author;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\Expr;
+
+/**
+ * @extends ServiceEntityRepository<Author>
+ *
+ * @method Author|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Author|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Author[]    findAll()
+ * @method Author[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AuthorRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Author::class);
+    }
+
+    public function findAuthor(): QueryBuilder
+    {
+        return $this->createQueryBuilder('a')
+            ->orderBy('a.name', 'ASC');
+    }
+}

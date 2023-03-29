a9280b25babbe0758940891f5a3e88476ef48d66
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-03-29 12:17:19 +0200

    fix: deconnexion fonctionnelle

--- frontlibrary/src/component/MyMenu.js
+++ frontlibrary/src/component/MyMenu.js
@@ -4,20 +4,25 @@ import { Link, useLocation } from "react-router-dom";
 function MyMenu() {
   const location = useLocation();
   const userId = localStorage.getItem("userId");
-  
-   
-  const connect= true;
-const routes = {
-  "/": "Accueil",
-  ...(connect && { "/amis": "Amis" }),
-  ...(connect && { "/profil": "Profil" }),
-  [connect ? "/deconnexion" : "/connexion"]: connect ? "Déconnexion" : "Connexion",
-};
+  var connect;
+  if (!userId) {
+    connect = false;
+  } else {
+    connect = true;
+  }
+  const routes = {
+    "/": "Accueil",
+    ...(connect && { "/amis": "Amis" }),
+    ...(connect && { "/profil": "Profil" }),
+    [connect ? "/deconnexion" : "/connexion"]: connect
+      ? "Déconnexion"
+      : "Connexion",
+  };
 
   const renderMenuItems = () => {
     return Object.entries(routes).map(([route, label]) => {
       const isActive = location.pathname === route;
-      const className = isActive ? "border-b-8 border-[#009999]": "";
+      const className = isActive ? "border-b-8 border-[#009999]" : "";
 
       return (
         <Link to={route} key={route} className={`p-4 ${className}`}>
@@ -32,7 +37,7 @@ const routes = {
       <div className="pt-14"></div>
       <header className="flex w-full justify-center fixed top-0 bg-white shadow z-50">
         <div className="flex flex-row justify-between items-center w-5/6 content-center">
-          <Link to={"/?userId="+userId}  className="text-blue">
+          <Link to={"/?userId=" + userId} className="text-blue">
             IUT - Bibliothèque
           </Link>
           <div className="flex space-x-4 bar-button">{renderMenuItems()}</div>

4cb2165fb942a133de3add3e55ea69614832d1dd
fvoneuw <felix.voneuw@etu.u-bordeaux.fr>
2023-03-29 12:00:23 +0200

    feat: commencement css book info

--- frontlibrary/src/component/MyFriends.js
+++ frontlibrary/src/component/MyFriends.js
@@ -3,7 +3,7 @@ import React from "react";
 function MyFriends() {
   return (
     <div className="bg-white py-16 text-left flex flex-col items-center">
-      <div className="md:w-[80%]">
+      <div className="w-[90%] md:w-[80%]">
         <h1 className="text-2xl">Mes amis</h1>
       </div>
     </div>

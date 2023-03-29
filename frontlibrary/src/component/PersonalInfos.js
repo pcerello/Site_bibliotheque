10127b69a4b0af24ed272465417c241aa2676675
fvoneuw <felix.voneuw@etu.u-bordeaux.fr>
2023-03-29 10:26:59 +0200

    feat: page amis commencé

--- frontlibrary/src/component/PersonalInfos.js
+++ frontlibrary/src/component/PersonalInfos.js
@@ -17,12 +17,18 @@ function PersonalInfos({ readerId }) {
     return <div>Loading...</div>;
   }
   return (
-    <div>
-      <h1>Mes informations personnelles</h1>
-      <p>Prénom : {reader.firstName}</p>
-      <p>Nom : {reader.lastName}</p>
-      <p>Adresse mail : {reader.email}</p>
-      <p>Ma photo : {reader.picture}</p>
+    <div className="mt-40 bg-white pt-8 flex flex-col items-center "> 
+      <div className="md:w-[80%]">
+        <h1 className="pb-4 text-2xl">Mes informations personnelles</h1>
+        <div className=" bg-white drop-shadow-md text-left ">
+          <div className="p-8">
+            <p>Prénom : {reader.firstName}</p>
+            <p>Nom : {reader.lastName}</p>
+            <p>Adresse mail : {reader.email}</p>
+            <p>Ma photo : {reader.picture}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

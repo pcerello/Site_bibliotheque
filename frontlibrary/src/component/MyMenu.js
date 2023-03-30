8d5aa5fde5bf669f7a5f564a3e067132a0de9faf
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-03-30 10:15:08 +0200

    feat: :sparkles: création menu burger

--- frontlibrary/src/component/MyMenu.js
+++ frontlibrary/src/component/MyMenu.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import logoIUT from "./logoIUT_Info.png";
+import { slide as Menu } from "react-burger-menu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function MyMenu() {
   const location = useLocation();
@@ -26,7 +28,12 @@ function MyMenu() {
       const className = isActive ? "border-b-8 border-[#009999]" : "";
 
       return (
-        <Link to={route} key={route} className={`p-4 ${className}`} arial-label={label} >
+        <Link
+          to={route}
+          key={route}
+          className={`p-4 ${className}`}
+          arial-label={label}
+        >
           {label}
         </Link>
       );
@@ -36,13 +43,32 @@ function MyMenu() {
   return (
     <>
       <div className="pt-14"></div>
-      <header className="flex w-full justify-center fixed top-0 bg-white shadow z-50">
+      <header className="flex w-full justify-center fixed top-0 bg-white shadow z-30">
         <div className="flex flex-row justify-between items-center w-5/6 content-center">
-          <Link to={"/"}  className="text-blue flex flex-row items-center" aria-label="IUT - Bibliothèque">
-            <img src={logoIUT} alt="logo iut info" className="max-h-[4vh] mr-2"/>
+          <Link to={"/"} className="p-4 text-blue flex flex-row items-center" aria-label="IUT - Bibliothèque">
+            <img
+              src={logoIUT}
+              alt="logo iut info"
+              className="max-h-[4vh] mr-2"
+            />
             IUT - Bibliothèque
           </Link>
-          <div className="flex space-x-4 bar-button">{renderMenuItems()}</div>
+          <div className="hidden md:flex space-x-4 bar-button">
+            {renderMenuItems()}
+          </div>
+          <div className="md:hidden">
+          <Menu
+            styles={{ bmMenu: { background: "#fff", opacity: 0.9 } }}
+            customBurgerIcon={
+              <div className="p-4">
+                <FontAwesomeIcon icon="fa-solid fa-bars" />
+              </div>
+            }
+            right
+            width={"100%"}>
+            {renderMenuItems()}
+          </Menu>
+        </div>
         </div>
       </header>
     </>
